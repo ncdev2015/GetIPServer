07de538ab105dc95f1d807260a5b1f8795e985be
ncdev2015 <ncdev2015@gmail.com>
2021-10-18 14:23:25 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/GetIPServerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetIPServer.Controllers
+{
+    public class GetIPServerController : Controller
+    {
+        // GET: GetIPServer
+        public string Index()
+        {
+            return Server.MachineName + "<br>" + Request.ServerVariables["LOCAL_ADDR"];
+        }
+    }
+}
